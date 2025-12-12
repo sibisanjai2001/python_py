8bcbf6d8581a65120fcc746f546d75efea440f48
sibisanjai R <sibisanjai741@gmail.com>
2025-12-12 18:12:33 +0530

    done

--- python learn -P/temp.py
+++ python learn -P/temp.py
@@ -1,4 +1,18 @@
-a=10
-for i in range(a):
-    print(i)
-print("Loop finished")
+class a:
+    def display(self):
+        print("Hello from class a")
+
+class b(a):
+    def display(self):
+        print("Hello from class b")
+
+class c(a):
+    def display(self):
+        print("Hello from class c")
+
+class d(b, c):
+    pass
+
+v=d()
+v.display()  # This will call the display method from class b due to MRO
+
