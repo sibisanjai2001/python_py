d7694a1afa9270d223d9be7c60dc339de0ef77fb
sibisanjai R <sibisanjai741@gmail.com>
2025-12-17 16:37:30 +0530

    today17

--- python learn -P/temp.py
+++ python learn -P/temp.py
@@ -1,18 +1,18 @@
-class a:
-    def display(self):
-        print("Hello from class a")
+class sibi:
+    def __init__(self, name):
+        self.name = name
 
-class b(a):
-    def display(self):
-        print("Hello from class b")
+    def greet(self):
+        return self.name + " says hello!"
+    
 
-class c(a):
-    def display(self):
-        print("Hello from class c")
+s=sibi("Sibi")
+s2=sibi("kavin")
 
-class d(b, c):
-    pass
+print(id(s))
+print(id(s2))
+print(id(s.name))
+print(id(s2.name))
 
-v=d()
-v.display()  # This will call the display method from class b due to MRO
-
+#    ❌ Misconception: You might think s.name "is part of" s, so they should share the same ID.
+ #   ✅ Reality: The instance s contains a reference to the string object s.name, but they are distinct objects in memory.
